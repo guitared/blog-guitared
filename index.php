615a90d117666baf0d0b106b4fae97c61bb2b9a8
Thatchapatt Kesornsri <guitared@users.noreply.github.com>
2016-11-25 18:04:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!doctype html>
+<meta charset="utf-8">
+<html>
+<head>
+<title>Guitared's blog</title>
+</head>
+<body>
+<header>
+  <b>Guitared's blog</b>
+</header>
+</body>
+<style>
+header {background: #ffcc00;}
+</style>
+</html>
